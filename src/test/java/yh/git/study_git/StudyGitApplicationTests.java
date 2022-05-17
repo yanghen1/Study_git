d30e1978981f7dc1780647818531d9d1663cd5e8
yh <2544978917@qq.com>
2022-05-17 14:24:41 +0800

    test edit

---
+++ src/test/java/yh/git/study_git/StudyGitApplicationTests.java
@@ -0,0 +1,13 @@
+package yh.git.study_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
