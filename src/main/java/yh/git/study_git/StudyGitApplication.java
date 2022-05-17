da6ba24898f916113735a33924da99844a1b910f
yh <2544978917@qq.com>
2022-05-17 14:20:43 +0800

    edit

---
+++ src/main/java/yh/git/study_git/StudyGitApplication.java
@@ -0,0 +1,13 @@
+package yh.git.study_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGitApplication.class, args);
+    }
+
+}
